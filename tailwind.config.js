d4b5680e1a0b6bf71a6208208a996dfe078afbb4
ramonespier <ramon.coelhomelo@outlook.com>
2024-11-20 11:00:11 -0300

    feat. mais medidas de grid-template-rows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,php}"],
   theme: {
     extend: {
-        colors: {
-          customColor: {
-            home: '#5e17eb',
-          },
+      colors: {
+        customColor: {
+          home: '#5e17eb',
         },
+      },
 
-        spacing: {
-          '128': '128px',
-          '-384': '-384px',
-          '768': '768px',
-          '-1921': '-1921px'
-        }
+      spacing: {
+        '128': '128px',
+        '-384': '-384px',
+        '768': '768px',
+        '-1921': '-1921px'
+      },
+
+      gridTemplateRows: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+        '14': 'repeat(14, minmax(0, 1fr))',
+        '15': 'repeat(15, minmax(0, 1fr))',
+        '16': 'repeat(16, minmax(0, 1fr))',
+        '17': 'repeat(17, minmax(0, 1fr))',
+        '18': 'repeat(18, minmax(0, 1fr))',
+        '19': 'repeat(19, minmax(0, 1fr))',
+        '20': 'repeat(20, minmax(0, 1fr))'
+      },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
 
