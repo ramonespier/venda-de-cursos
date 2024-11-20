0e8866819718016db25a2d58a5b0b688f905d2f6
ramonespier <ramon.coelhomelo@outlook.com>
2024-11-20 13:58:14 -0300

    chore. adições no menu (pensando)

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function criarHeader() {
         grid grid-cols-2 grid-rows-1 md:gap-80 gap-32 md:justify-items-stretch justify-items-center">
 
             <div class="md:flex md:items-center col-start-1 col-span-1 md:ml-20 w-24">
-                <img src="./img/FocusHub.png" class="w-24" alt="Logotipo do site FocusHub">
+                <a href="./index.html"><img src="./img/FocusHub.png" class="w-24" alt="Logotipo do site FocusHub"></a>
             </div>
 
             <div class="flex items-center justify-end md:col-start-2 md:col-span-1
@@ -20,12 +20,25 @@ function criarHeader() {
             </div>
         </nav>
 
-        <div id="menu" class=" grid grid-cols-1 grid-rows-12
+        <div id="menu" class=" grid grid-cols-1 grid-rows-16
         h-full w-screen md:w-96 bg-violet-950
         fixed top-0 md:-right-384">
+
             <span class="row-start-1 text-white text-3xl cursor-pointer underline
             flex items-center ml-5
             md:hidden" id="fecharMenu">Fechar</span>
+
+            <div class="row-start-3 row-span-2 w-full
+            flex flex-col justify-center items-center
+            
+            md:row-start-1 md:row-end-2">
+                <div class="bg-gray-600 w-24 h-24 rounded-full"></div>
+            </div>
+
+            <span class="row-start-5 text-white text-3xl cursor-default
+            flex items-center justify-center
+            md:row-start-3">Usuário</span>
+
         </div>
 
         `;
