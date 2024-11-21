0cc1747b79db16617320a69b0f97caa23f5a4016
ramonespier <ramon.coelhomelo@outlook.com>
2024-11-20 21:09:18 -0300

    style: ajuste de tamanho do footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,36 +1,35 @@
 function criarFooter() {
     const footer = document.createElement('footer');
     footer.innerHTML = `
-        <div class="md:h-64 h-80 w-full bg-customColor-home">
+        <div class="md:h-64 h-40 w-full bg-customColor-home">
             <div class="grid w-full h-full
-            grid-rows-4 grid-cols-2
+            grid-rows-3 grid-cols-2
             md:grid-rows-2 md:grid-cols-3">
 
                 <div class="w-full
-                md:col-start-1 md:col-end-1
-                row-start-1 col-start-1 col-span-2
-                flex justify-center items-center">
+                md:col-start-1 md:col-end-1 md:flex md:justify-center
+                hidden">
                     <img src="./img/FocusHub.png" class="md:w-128 w-20" alt="">
                 </div>
 
                 <div class="md:col-start-3 md:col-end-3 md:row-start-1 md:row-end-1
-                row-start-2 col-start-1 col-span-2
+                row-start-1 col-start-1 col-span-2
                 text-white font-bold
                 flex justify-center items-center">
                     <span class="text-3xl">Enjoy your Journey!</span>
                 </div>
 
                 <div class="md:row-start-2 md:row-end-2 md:col-start-1 md:col-end-1
-                row-start-3 row-end-3 col-start-1 col-span-2
+                row-start-2 col-start-1 col-span-2
                 font-bold text-white text-center
                 flex justify-evenly items-center md:justify-evenly w-full p-2">
-                <a class="w-12" href="#"><img src="./img/instagram.png" alt="Instagram"></a>
-                <a class="w-12" href=""><img src="./img/twitter.png" alt="X"></a>
-                <a class="w-12" href=""><img src="./img/linkedin.png" alt="LinkedIn"></a>
+                <a class="md:w-12 w-10" href="#"><img src="./img/instagram.png" alt="Instagram"></a>
+                <a class="md:w-12 w-10" href=""><img src="./img/twitter.png" alt="X"></a>
+                <a class="md:w-12 w-10" href=""><img src="./img/linkedin.png" alt="LinkedIn"></a>
                 </div>
 
                 <div class="md:row-start-2 md:row-end-2 md:col-start-2 md:col-end-2
-                row-start-4 row-end-4 col-start-1 col-span-2
+                row-start-3 col-start-1 col-span-2
                 w-full text-white p-2
                 flex items-end justify-center">
                     <p>&copy; FocusHub. Todos os direitos reservados.</p>
