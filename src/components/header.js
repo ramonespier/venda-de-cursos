ba3563d2eb83bf6cd9f55874226042ead36abfc9
ramonespier <ramon.coelhomelo@outlook.com>
2024-11-28 21:56:49 -0300

    FEAT. NOME DE USUARIO NO MENU

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,19 @@
 function criarHeader() {
     const header = document.createElement('header');
 
-    let usuario //= `guest${Math.floor(Math.random() * 1000) + 1}`;
-
-    fetch('http://localhost/24250469/venda-de-cursos/src/php/session_dataa.php')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Username from AJAX: ' +  data.usuario);
-        usuario = data.usuario
-    })
-    .catch(error => console.error('Error:', error))
-
     header.innerHTML = `
-        <nav class="md:h-24 h-24 bg-customColor-home w-full
+        <nav class="md:h-32 h-24 bg-customColor-home w-full
         grid grid-cols-2 grid-rows-1 md:gap-80 gap-32 md:justify-items-stretch justify-items-center">
 
-            <div class="md:flex md:items-center col-start-1 col-span-1 md:ml-20 w-24">
-                <a href="../php/index.php"><img src="../img/FocusHub.png" class="w-24" alt="Logotipo do site FocusHub"></a>
+            <div class="md:flex md:items-center col-start-1 col-span-1 md:ml-20 w-full">
+                <a href="../php/index.php"><img src="../img/FocusHub.png" class="md:w-32 w-24" alt="Logotipo do site FocusHub"></a>
             </div>
 
             <div class="flex items-center justify-end md:col-start-2 md:col-span-1
             text-white md:text-5xl md:m-2">
 
                 <div class="grid grid-cols-1 grid-rows-3 gap-1 md:mr-20 cursor-pointer p-4
-                bg-violet-800 rounded-md" id="abreMenu">
+                bg-indigo-950 rounded-md" id="abreMenu">
                     <div class="bg-white w-7 h-1 row-start-1"></div>
                     <div class="bg-white w-7 h-1 row-start-2"></div>
                     <div class="bg-white w-7 h-1 row-start-3"></div>
@@ -32,17 +22,17 @@ function criarHeader() {
         </nav>
 
         <div id="menu" class=" grid grid-cols-1 grid-rows-16
-        h-full w-screen md:w-96 bg-violet-950
-        fixed top-0 md:-right-384 border-l-2 border-l-black">
+        h-full w-screen md:w-96 bg-gradient-to-b from-customColor-home via-indigo-900 to-customColor-home
+        fixed top-0 md:-right-384 border-l-2 border-l-slate-400">
 
             <span class="row-start-1 text-white text-3xl cursor-pointer
             flex items-center pl-4 transition hover:bg-red-600 hover:border-t hover:border-b
             md:hidden" id="fecharMenu">✕ Fechar</span>
 
             <span class="row-start-2 text-white text-3xl cursor-default
-            flex items-center justify-center
-            md:row-start-1">
-            ${usuario}
+            flex items-center justify-center 
+            md:row-start-1" id="usuario">
+                Convidado
             </span>
 
             <a href="./login.php" 
@@ -50,9 +40,14 @@ function criarHeader() {
             md:row-start-3 transition flex items-center 
             hover:border-t hover:border-b pl-4 hover:bg-customColor-menuhov">Faça seu login</a>
 
-            <a href="./patch.php" 
+            <a href="./logout.php" 
             class="text-white text-3xl row-start-5
             md:row-start-4 transition flex items-center 
+            hover:border-t hover:border-b pl-4 hover:bg-customColor-menuhov">Excluir conta logada</a>
+
+            <a href="./patch.php" 
+            class="text-white text-3xl row-start-6
+            md:row-start-5 transition flex items-center 
             hover:border-t hover:border-b pl-4 hover:bg-customColor-menuhov">Notas de atualização</a>
 
             <a href="#" 
@@ -100,8 +95,23 @@ function adicionarHeader() {
         menu.style.right = '-768px';
         abreMenu.style.transition = '0.5s';
         abreMenu.style.transform = 'translateX(0)'
+    })
+
+    fetch('session_data.php')
+        .then(response => response.json())
+        .then(data => {
+            if (data.username) {
+                const usuario = document.getElementById('usuario');
+                if (usuario) {
+                    usuario.innerText = data.username;
+
+                }
+            } else {
+                console.error('Erro:', data.error);
+            }
         })
-    }
+        .catch(error => console.error('Erro:', error));
+}
 
 if (window.onload) {
     let prevOnload = window.onload;
---
+++ src/php/session_data.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+header('Content-Type: application/json');
+if(isset($_SESSION['username'])) {
+        echo json_encode(['username' => $_SESSION['username']]);
+} else {
+        echo json_encode(['error' => 'Usuário não logado.', 'session' => $_SESSION]);
+}
