f1a7118771e8d738b0c1cf66f90d30a8e34e2a61
ramonespier <ramon.coelhomelo@outlook.com>
2024-11-30 11:28:26 -0300

    estilo do certificado

--- src/php/aprova.php
+++ src/php/aprova.php
@@ -47,105 +47,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="../output.css">
     <link rel="stylesheet" href="../scrollbar.css">
     <link rel="shortcut icon" href="../img/FocusHub.png" type="image/x-icon">
-
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-
-            
-            background-color: #f3f3f3;
-            margin: 0;
-        }
-
-        .certificado {
-            width: 800px;
-            height: 600px;
-            border: 10px solid #000;
-            padding: 30px;
-            background-color: #fff;
-            text-align: center;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-            position: relative;
-        }
-
-        .topo {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .logo img {
-            width: 100px;
-            height: auto;
-        }
-
-        .titulo {
-            font-size: 2.5em;
-            font-weight: bold;
-            margin: 0;
-            text-transform: uppercase;
-        }
-
-        .texto {
-            font-size: 1.2em;
-            margin: 20px 0;
-        }
-
-        .nome {
-            font-size: 1.5em;
-            font-weight: bold;
-            margin: 30px 0;
-            text-transform: uppercase;
-            letter-spacing: 1.5px;
-        }
-
-        .rodape {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            position: absolute;
-            bottom: 30px;
-            left: 30px;
-            right: 30px;
-        }
-
-        .assinatura img {
-            width: 150px;
-            height: auto;
-        }
-
-        .assinatura p {
-            margin: 5px 0;
-        }
-
-        .data {
-            text-align: right;
-            font-size: 1em;
-        }
-    </style>
 </head>
 
-<body class="w-full bg-gradient-to-b from-indigo-950 via-indigo-900 to-indigo-950 font-josefin flex flex-col">
+<body class="w-full h-screen bg-gradient-to-b from-indigo-950 via-indigo-900 to-indigo-950 font-josefin flex flex-col">
 
     <div id="header"></div>
 
-    <div class="certificado">
-        <div class="topo">
-            <div class="titulo"> Certificado de Participação </div>
-        </div>
-        <div class="texto"> Este certificado é concedido a </div>
-        <div class="nome"> <?php echo htmlspecialchars($nome); ?> </div>
-        <div class="texto"> em reconhecimento à sua participação no curso de <?php foreach($cursos as $key => $curso) { if ($key == $index) { echo $curso['nome']; } } ?>. </div>
-        <div class="rodape">
-            <div class="assinatura"> <img src="../img/FocusHub.png" alt="Assinatura">
+    <main class="grid grid-rows-6 grid-cols-6 h-screen">
+        <div class="certificado row-start-2 row-span-4 col-start-2 col-span-4
+        grid grid-rows-6 grid-cols-5 justify-items-center
+        border-4 bg-white border-[#FFD700] p-5">
+            <div class="col-start-2 col-span-3 row-start-1 text-3xl font-extrabold self-center"> Certificado de conclusão de curso</div>
+            <div class="row-start-3 col-start-2 col-span-3 text-center text-lg self-center"> Este certificado é concedido a </div>
+            <div class="row-start-4 col-start-3 text-center text-2xl font-bold"> <?php echo htmlspecialchars($nome); ?> </div>
+            <div class="row-start-5 col-start-1 col-span-5 text-xl"> em reconhecimento à sua participação no curso de <?php foreach ($cursos as $key => $curso) {
+                                                                                                                            if ($key == $index) {
+                                                                                                                                echo $curso['nome'];
+                                                                                                                            }
+                                                                                                                        } ?>. </div>
+            <div class="row-start-1 text-center font-bold text-lg">
+                <img src="../img/FocusHub.png" width="100px" alt="Assinatura">
                 <p>FocusHub</p>
             </div>
-            <div class="data">
-                <p>Data: <?php echo $conclusao ?></p>
-            </div>
+            <p class="row-start-6 col-start-5 self-end text-lg">Data: <?php echo $conclusao ?></p>
+
         </div>
-    </div>
-    <button onclick="gerarPDF()">Baixar Certificado</button>
+        <button onclick="gerarPDF()" class="row-start-6 col-start-3 col-span-2 text-3xl bg-[#FFD700] m-10 rounded-xl hover:scale-110 transition font-extrabold">Baixar Certificado!</button>
+    </main>
 
 
     <div id="footer"></div>
@@ -154,29 +82,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <script src="../components/header.js"></script>
     <script src="../components/footer.js"></script>
 
-        <!-- BIBLIOTECA PARA TRANSFORMAR HTML EM PDF (jsPDF e html2canvas) -->
+    <!-- BIBLIOTECA PARA TRANSFORMAR HTML EM PDF (jsPDF e html2canvas) -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js"></script>
 
-<script>
-    function gerarPDF() {
-        const { jsPDF } = window.jspdf; // a funcao acessa a biblioteca jsPDF a partir do objeto window.jspdf
-        const doc = new jsPDF({ // um novo objeto jsPDF é criado. este objeto eh utilizado para gerar o PDF
-            orientation: 'landscape', // define a orientação do documento. dode ser 'portrait' (retrato) ou 'landscape' (paisagem)
-            unit: 'px', // define a unidade de medida para o documento. no meu caso 'mm'
-            format: '1920, 1080' // tamanho do documento
-        });
+    <script>
+        function gerarPDF() {
+            const {
+                jsPDF
+            } = window.jspdf; // a funcao acessa a biblioteca jsPDF a partir do objeto window.jspdf
+            const doc = new jsPDF({ // um novo objeto jsPDF é criado. este objeto eh utilizado para gerar o PDF
+                orientation: 'landscape', // define a orientação do documento. dode ser 'portrait' (retrato) ou 'landscape' (paisagem)
+                unit: 'px', // define a unidade de medida para o documento. no meu caso 'mm'
+                format: [1300, 840] // tamanho do documento
+            });
 
-        // adicionar o conteúdo html ao PDF
-        doc.html(document.querySelector('.certificado'), {
-            callback: function (doc) { // funcao de 'callback' que eh chamada quando a conversão do HTML para PDF eh concluida.
-                doc.save('<?php foreach($cursos as $key => $curso) { if ($key == $index) { echo $curso['nome']; } } ?>.pdf'); // qnd baixar o PDF ele terá esse nome
-            },
-            x: 20,
-            y: 20
-        });
-    }
-</script>
+            // adicionar o conteúdo html ao PDF
+            doc.html(document.querySelector('.certificado'), {
+                callback: function(doc) { // funcao de 'callback' que eh chamada quando a conversão do HTML para PDF eh concluida.
+                    doc.save('<?php foreach ($cursos as $key => $curso) { if ($key == $index) { echo $curso['nome']; } } ?>.pdf'); // qnd baixar o PDF ele terá esse nome
+                },
+                x: 10,
+                y: 10
+            });
+        }
+    </script>
 
 
 </body>
